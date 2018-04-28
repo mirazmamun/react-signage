b509b736cc7ba0f0c055eed2d8c896650500bd90
Miraz <mirazalmamun@gmail.com>
2018-04-29 00:34:44 +1000

    fix: animation

--- src/App.js
+++ src/App.js
@@ -7,24 +7,20 @@ import bootstrapCss from 'bootstrap/dist/css/bootstrap.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {bannerDigit: 'A'};
+    this.state = {bannerDigit: 'A', blinkInterval: 3};
 
   }
   keyUp = (e) => {
-    console.log(e.target.value);
-    this.setState({bannerDigit: 'B'});
+    this.setState({bannerDigit: e.target.value ? e.target.value.toUpperCase() : '0'});
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Digital Signage Playground</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div style={{fontSize: '6em'}}>
+        <div style={{fontSize: '6em'}} className="banner-Transform">
           {this.state.bannerDigit}
         </div>
         <input type="text" placeholder="Type in banner text" onKeyUp={this.keyUp}/>
